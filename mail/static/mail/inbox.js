359192576352ddaa708aa4deab512e90f2216cd6
Bassel Elsheikh <bassel.elsheikh@gmail.com>
2022-12-27 13:59:49 +0200

    get mailbox
    still need to handle error

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -21,6 +21,7 @@ function compose_email() {
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
+  document.querySelector('#compose-alert').textContent = '';
 }
 
 function load_mailbox(mailbox) {
@@ -28,13 +29,45 @@ function load_mailbox(mailbox) {
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#compose-alert').style.display = 'none';
 
   // Show the mailbox name
-  document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+  document.querySelector("#mailbox-name").innerHTML=`<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+  get_mailbox(mailbox);
+}
+
+async function get_mailbox(mailbox) {
+  try {
+    const response = await fetch(`/emails/${mailbox}`);
+    if (!response.ok) {
+      const result = await response.json();
+      throw new Error(`HTTP error: ${response.status}`);
+    }
+    const result = await response.json();
+    for (const email of result) {
+      const emailItem = document.createElement('li');
+      emailItem.classList.add('list-group-item');
+      const sender = document.createElement('strong');
+      sender.innerHTML = `${email["sender"]}</br>`;
+      emailItem.appendChild(sender);
+      const subject = document.createTextNode(`${email["subject"]}`);
+      emailItem.appendChild(subject);
+      timestamp = document.createElement('p');
+      timestamp.classList.add('text-muted');
+      timestamp.innerHTML = `${email["timestamp"]}`;
+      emailItem.appendChild(timestamp);
+      document.querySelector("#emails-list").appendChild(emailItem);
+    }
+  }
+  catch (error) {
+    console.error(`Could not retrieve mailbox: ${error}`);
+  }
+
+
 }
 
 function send_email_handler(event) {
-  send_email()
+  send_email();
   //prevent form default submitting behaviour
   event.preventDefault();
 }
@@ -53,7 +86,9 @@ async function send_email(event) {
     })
     if (!response.ok) {
       const result = await response.json();
-      document.querySelector('#compose-recipients').value=result["error"];
+      alert = document.querySelector('#compose-alert')
+      alert.style.display = 'block';
+      alert.textContent = result["error"];
       throw new Error(`HTTP error: ${response.status}`);
     }
     const result = await response.json();
@@ -62,7 +97,7 @@ async function send_email(event) {
   catch (error) {
     console.error(`Could not send email: ${error}`);
   }
-  
+
 
 
 }
